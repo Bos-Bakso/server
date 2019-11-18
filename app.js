1dcd52fc47a38567deb2e3acbee32508c7f0a43b
aldinofrizal <aldinofrizal@gmail.com>
2019-11-18 13:50:00 +0700

    rank

--- app.js
+++ app.js
@@ -16,23 +16,23 @@ const rank = require('./helper/rank')
 mongo()
 
 app.use(cors())
-io.on('connect', async(socket) => {
-    console.log('connect socket yeay ')
-    let data = await rank() 
-    socket.on('add', function(){
-        io.emit('test' ,  data )   
-    })
-    io.emit('test' ,  data )
+// io.on('connect', async(socket) => {
+//     console.log('connect socket yeay ')
+//     let data = await rank() 
+//     socket.on('add', function(){
+//         io.emit('test' ,  data )   
+//     })
+//     io.emit('test' ,  data )
 
-    socket.on('disconnect', function(){
-        socket.disconnect()
-    })
-})
+//     socket.on('disconnect', function(){
+//         socket.disconnect()
+//     })
+// })
 
-app.use((req, res, next) => {
-    req.io = io
-    next()
-})
+// app.use((req, res, next) => {
+//     req.io = io
+//     next()
+// })
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ const router = require('express').Router()
 const user = require('./user')
 const transaction = require('./transaction')
 const service = require('./services')
+const rank = require('../helper/rank')
 
 router.use('/user', user)
 router.use('/transaction', transaction)
 router.use('/service', service)
+router.get('/rank',  async (req,res) => {
+    let data = await rank()
+    res.status(200).json({ rank : data })
+})
 
 module.exports = router
