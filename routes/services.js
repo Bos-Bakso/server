1fa84194a10c260e6dcf005432f3a77bf8bf0709
aldinofrizal <aldinofrizal@gmail.com>
2019-11-18 13:53:20 +0700

    modify service patch

--- routes/services.js
+++ routes/services.js
@@ -3,7 +3,7 @@ const Controller = require('../controller/service')
 const { authentication, authorization } = require('../middleware/auth')
 
 router.post('/', authentication ,Controller.addService)
-router.patch('/:id', authentication, authorization, Controller.updateService)
+router.patch('/:id', authentication, Controller.updateService)
 router.get('/', authentication, Controller.find)
 
 
